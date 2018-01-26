5a3b1175f0cf9f1f54cfdcba92fd971d82e62b5a
Alexander <alexbrianzhang@gmail.com>
2018-01-26 08:34:26 -0500

    Improve AWS worker starting script

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn app:app --config=gunicorn_config.py
+start_worker: python start_worker.py
--- start_worker.py
+++ start_worker.py
@@ -4,8 +4,11 @@ import boto3, os
 # Check if messages in queue
 sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName='kbrenders-queue.fifo')
-print(queue.attributes)
-if queue.attributes['ApproximateNumberOfMessages'] > 0:
+num_messages = int(queue.attributes['ApproximateNumberOfMessages'])
+if num_messages > 0:
     # start EC2 instance to process queue (will shut itself down afterwards)
     ec2 = boto3.resource('ec2')
     instance = ec2.Instance(os.environ['AWS_INSTANCE_ID'])
+    instance.start()
+    print('Start signal sent to instance '+os.environ['AWS_INSTANCE_ID'])
+    print('Processing '+str(num_messages)+' messages')

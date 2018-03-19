8f7a31a1804d1321389452384acd7b4b16a85d71
Alexander <alexbrianzhang@gmail.com>
2018-03-19 19:16:53 -0400

    Add Klippe (60%)

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-from flask import Flask, render_template, redirect, flash, request, Markup
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import StringField, SelectField, HiddenField
-from wtforms_components import ColorField
-from wtforms.validators import DataRequired
-from flask_sslify import SSLify
 import os, json, glob, markdown, stripe, boto3
+import flask_wtf, flask_sslify, wtforms, wtforms_components, wtforms.validators
+from flask import Flask, render_template, redirect, flash, request, Markup
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 app = Flask(__name__)
-sslify = SSLify(app)
+sslify = flask_sslify.SSLify(app)
 app.config.from_object('config')
 stripe.api_key = os.environ['STRIPE_SECRET_KEY']
 sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName='kbrenders-queue.fifo')
-
 
 about_text = '''
 ### What does kbrenders do?
@@ -33,38 +28,66 @@ Service outages, changelogs, and other information will be posted on the **[geek
 *Many thanks to RAMA, Mech27, and Mechkeys.ca for their keyboard models.*
 '''
 
-templates = [('M65', 'SA', 'http://www.keyboard-layout-editor.com/#/gists/3ca3649e1d048134ddd0e835d1dd735b'),
-        ('M65', 'DSA', 'http://www.keyboard-layout-editor.com/#/gists/3ca3649e1d048134ddd0e835d1dd735b'),
-        ('M65', 'GMK', 'http://www.keyboard-layout-editor.com/#/gists/4319599274157d2a0dd0e38328b76878'),
-        ('Mech27', 'SA', 'http://www.keyboard-layout-editor.com/#/gists/10629d008a99d8d6eb6f8c59414b5dd8'),
-        ('Mech27', 'DSA', 'http://www.keyboard-layout-editor.com/#/gists/10629d008a99d8d6eb6f8c59414b5dd8'),
-        ('Mech27', 'GMK', 'http://www.keyboard-layout-editor.com/#/gists/6e6692825b348f40c040ca9750e469a8'),
-        ('MM2', 'SA', 'http://www.keyboard-layout-editor.com/#/gists/ea2a231112ffceae047494ac9a93e706'),
-        ('MM2', 'DSA', 'http://www.keyboard-layout-editor.com/#/gists/ea2a231112ffceae047494ac9a93e706'),
-        ('MM2', 'GMK', 'http://www.keyboard-layout-editor.com/#/gists/eed1f1854dda3999bcdd730f0143c627'),
-        ('Espectro', 'SA', 'http://www.keyboard-layout-editor.com/#/gists/6b996bea3ebf8a85866ddea606e25de4'),
-        ('Espectro', 'DSA', 'http://www.keyboard-layout-editor.com/#/gists/6b996bea3ebf8a85866ddea606e25de4'),
-        ('Espectro', 'GMK', 'http://www.keyboard-layout-editor.com/#/gists/6a03012a82e7bbca14db635142913a7f')]
+templates = [
+    ('MM2', 'SA', 'http://www.keyboard-layout-editor.com/#/gists/ea2a231112ffceae047494ac9a93e706'),
+    ('MM2', 'DSA', 'http://www.keyboard-layout-editor.com/#/gists/ea2a231112ffceae047494ac9a93e706'),
+    ('MM2', 'GMK', 'http://www.keyboard-layout-editor.com/#/gists/eed1f1854dda3999bcdd730f0143c627'),
+    ('Klippe', 'SA', 'http://www.keyboard-layout-editor.com/#/gists/f8369e8d6ae12c6d30bbf6db9731bca5'),
+    ('Klippe', 'DSA', 'http://www.keyboard-layout-editor.com/#/gists/f8369e8d6ae12c6d30bbf6db9731bca5'),
+    ('Klippe', 'GMK', 'http://www.keyboard-layout-editor.com/#/gists/c2aedbf20e6a1ee5320a0f89b114d6da'),
+    ('M65', 'SA', 'http://www.keyboard-layout-editor.com/#/gists/3ca3649e1d048134ddd0e835d1dd735b'),
+    ('M65', 'DSA', 'http://www.keyboard-layout-editor.com/#/gists/3ca3649e1d048134ddd0e835d1dd735b'),
+    ('M65', 'GMK', 'http://www.keyboard-layout-editor.com/#/gists/4319599274157d2a0dd0e38328b76878'),
+    ('Mech27', 'SA', 'http://www.keyboard-layout-editor.com/#/gists/10629d008a99d8d6eb6f8c59414b5dd8'),
+    ('Mech27', 'DSA', 'http://www.keyboard-layout-editor.com/#/gists/10629d008a99d8d6eb6f8c59414b5dd8'),
+    ('Mech27', 'GMK', 'http://www.keyboard-layout-editor.com/#/gists/6e6692825b348f40c040ca9750e469a8'),
+    ('Espectro', 'SA', 'http://www.keyboard-layout-editor.com/#/gists/6b996bea3ebf8a85866ddea606e25de4'),
+    ('Espectro', 'DSA', 'http://www.keyboard-layout-editor.com/#/gists/6b996bea3ebf8a85866ddea606e25de4'),
+    ('Espectro', 'GMK', 'http://www.keyboard-layout-editor.com/#/gists/6a03012a82e7bbca14db635142913a7f')
+]
+
+email_text = "We'll email the final render to this address within 24 hours."
+t_str = '<a id="{0}_{1}" class="template" target="_blank" href="{2}">{1} on {0}</a>'
+kle_text = '<b>You MUST use this template: ' + ' '.join(t_str.format(*t) for t in templates) + '</b><br/>'
+kle_text += 'You can preview legend appearance using <a href="http://kle-render.herokuapp.com">kle-render</a>'
 
 
-class OrderForm(FlaskForm):
-    email = StringField('Email Address', validators=[DataRequired()],
-            description="We'll email the final render to this address within 24 hours.")
-    keyboard = SelectField('Keyboard', choices=[('MM2', 'Mech Mini 2 (40%)'), ('M65', 'M65-A (65%)'), ('Mech27', 'Mech27 (TKL)'), ('Espectro', 'Espectro (96%)')])
-    profile = SelectField('Keycap Profile', choices=[('SA', 'SA'), ('GMK', 'GMK'), ('DSA', 'DSA')])
-    kle = FileField('KLE JSON', validators=[FileRequired(), FileAllowed(['json'], 'Upload must be JSON')],
-            description='<b>You MUST use this template: {}</b><br/>You can preview legend appearance using <a href="http://kle-render.herokuapp.com">kle-render</a>'.format(
-                ' '.join(['<a id="{}" class="template" target="_blank" href="{}">{}</a>'.format(t[0]+'_'+t[1], t[2], t[1]+' on '+t[0]) for t in templates])))
-    camera = SelectField('Camera Angle', choices=[('Side', 'Side View'), ('Top', 'Top View'), ('Front', 'Front View')])
-    background = ColorField('Background', default='#ffffff')
-    stripeToken = HiddenField('stripeToken')
+class OrderForm(flask_wtf.FlaskForm):
+    email = wtforms.StringField('Email Address',
+        validators=[wtforms.validators.DataRequired()],
+        description=email_text
+    )
+    keyboard = wtforms.SelectField('Keyboard', choices=[
+        ('MM2', 'Mech Mini 2 (40%)'),
+        ('Klippe', 'Klippe (60%)'),
+        ('M65', 'M65-A (65%)'),
+        ('Mech27', 'Mech27 (TKL)'),
+        ('Espectro', 'Espectro (96%)')
+    ])
+    profile = wtforms.SelectField('Keycap Profile', choices=[
+        ('SA', 'SA'),
+        ('GMK', 'GMK'),
+        ('DSA', 'DSA')
+    ])
+    kle = FileField('KLE JSON', validators=[
+        FileRequired(),
+        FileAllowed(['json'], 'Upload must be JSON')
+    ], description=kle_text)
+    camera = wtforms.SelectField('Camera Angle', choices=[
+        ('Side', 'Side View'),
+        ('Top', 'Top View'),
+        ('Front', 'Front View')
+    ])
+    background = wtforms_components.ColorField('Background', default='#ffffff')
+    stripeToken = wtforms.HiddenField('stripeToken')
+
 
 def add2queue(message):
     message['background'] = message['background'].hex
     message['kle'] = message['kle'].read().decode('utf-8')
-
     queue.send_message(MessageBody=json.dumps(message), MessageGroupId='render_request')
     flash('Your order has been queued.')
+
 
 def charge_card(token):
     charge = stripe.Charge.create(
@@ -75,25 +98,35 @@ def charge_card(token):
     )
     return charge
 
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = OrderForm()
-    images = [('GMK Carbon on Mech Mini 2, Top View', 'MM2_Top.png'),
-            ('DSA Lunchbar on M65-A, Side View', 'M65_Side.png'),
-            ('GMK Carbon on Mech27, Side View', 'Mech27_Side.png'),
-            ('SA Lunchbar on Espectro, Front View', 'Espectro_Front.png')]
+    images = [
+        ('GMK Carbon on Mech27, Side View', 'Mech27_Side.png'),
+        ('SA Lunchbar on Espectro, Front View', 'Espectro_Front.png'),
+        ('GMK Carbon on Mech Mini 2, Top View', 'MM2_Top.png'),
+        ('DSA Lunchbar on M65-A, Side View', 'M65_Side.png'),
+        ('GMK Carbon on Klippe, Front View', 'Klippe_Front.png')
+    ]
     if not form.validate_on_submit():
         if request.method == 'POST':
             flash('There was in error in your order form. Your card was not charged.')
         about_rendered = Markup(markdown.markdown(about_text))
-        return render_template('index.html', images=images, about_text=about_rendered,
-                form=form, stripeKey=os.environ['STRIPE_PUBLISHABLE_KEY'])
+        return render_template(
+            'index.html',
+            images=images,
+            about_text=about_rendered,
+            form=form,
+            stripeKey=os.environ['STRIPE_PUBLISHABLE_KEY']
+        )
     if charge_card(form.data['stripeToken']):
         add2queue({i:form.data[i] for i in form.data if i != 'csrf_token' and i != 'stripeToken'})
     else:
         flash('Your payment could not be processed. Your card was not charged.')
     return redirect('/')
 
+
 if __name__ == '__main__':
-    app.run(debug=True) # Use debug=False on actual server
+    app.run(debug=True)
